56deee5249b7f9316e15a13ca4e18cbc68133167
simo <simostanga@libero.it>
2024-09-02 19:56:07 +0200

    bug in broadcast

--- src_omp/main.cpp
+++ src_omp/main.cpp
@@ -13,6 +13,16 @@ struct Task_t {
     uint64_t j;
 };
 
+void print_M (std::vector<double> *M, uint64_t N) {
+    for (uint64_t i=0; i<N; ++i) {
+        std::printf("\n");
+        for (uint64_t j=0; j<N; ++j){
+            std::printf("%f  ",(*M)[i*N+j]);
+        }
+    }
+    std::printf("\n");
+}
+
 uint64_t index_compute (uint64_t n) {
 	uint64_t res = 0;
     for (uint64_t i=0; i<n; ++i)
@@ -26,7 +36,7 @@ void init_M (std::vector<double> *M, uint64_t N) {
     }
 }
 
-bool compare_M (std::vector<double> *A,std::vector<double> *B) {
+bool compare_M (std::vector<double> *A,std::vector<double> *B, uint64_t N) {
     bool c = true;
     for (uint64_t i=0; i<N; ++i) 
         for (uint64_t j=0; j<N; ++j)
@@ -59,15 +69,19 @@ int main (int argc, char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD,&myId);
 
 	uint64_t N = std::stol(argv[1]);
-	std::vector<double> M(N * N, 0);
 
-	init_M(&M,N);
+
+
 
 #ifdef SEQ 
-	compute_seq(&M,N);
+	std::vector<double> M_test(N * N, 0);
+	init_M(&M_test,N);
+	compute_seq(&M_test,N);
 #endif
 
 	if (myId == 0) { /* task emitter */
+		std::vector<double> M(N * N, 0);
+		init_M(&M,N);
 
 		//std::printf("Emitter Online\n");
 
@@ -75,7 +89,8 @@ int main (int argc, char *argv[]){
 		uint64_t tasks[N*2];
 		std::vector<MPI_Request> requests_send(n_Nodes-1, 0);
 		std::vector<MPI_Request> requests_receive(N, 0);
-		double result[N-1];
+		std::vector<double> result(N-1, 0);
+		//double result[N-1];
 
 		for (uint64_t i=1; i<N; ++i){ 
         	for (uint64_t j=0; j<N-i; ++j){ /* e^k_(i,j) */ 
@@ -113,26 +128,39 @@ int main (int argc, char *argv[]){
 				MPI_Wait(&requests_receive[j], MPI_STATUS_IGNORE);
 	 		//std::printf("RISULTATI arrivati \n");
 
-        	for (uint64_t j=0; j<N-i; ++j) /* buffer creation for the broadcast */ 
+        	for (uint64_t j=0; j<N-i; ++j) {/* buffer creation for the broadcast */ 
 				M[j*N+j+i] = result[j];
+			}
 
 			MPI_Request requests_broadcast;
-			MPI_Ibcast(result, N-i, MPI_DOUBLE, myId, MPI_COMM_WORLD, &requests_broadcast);
+			MPI_Ibcast(&result[0], N-i, MPI_DOUBLE, myId, MPI_COMM_WORLD, &requests_broadcast);
 			MPI_Wait(&requests_broadcast, MPI_STATUS_IGNORE);
+			sleep(1);
+			result.clear();
+			result.resize(N-i-1);
 	 		//std::printf("BROADCAST ARRIVATO \n");
 		}
 
 	 	//std::printf("EMITTER ESCE \n");
+#ifdef SEQ
+    if (compare_M(&M,&M_test,N)) 
+        std::printf("Test Passed\n");
+    else
+        std::printf("Test Failed\n");
+#endif
 
 	} else { /* worker Node */
+	 	std::vector<double> M(N * N, 0);
+		init_M(&M,N);
 
 		int ready_receive;
 		int ready_broadcast;
 		bool c = true;
-		double res = 99;
+		double res = 0;
 		uint64_t n_broadcast = 0;
 		uint64_t task[2] = {0,0};
-    	double result[N-1];
+    	//double result[N-1];
+		std::vector<double> result(N-1, 0);
 
 		while (n_broadcast < N-1) {
 			MPI_Request requests_receive;
@@ -141,45 +169,72 @@ int main (int argc, char *argv[]){
 				MPI_Irecv(task, 2, MPI_UINT64_T, 0, 0, MPI_COMM_WORLD, &requests_receive);
 				c = true;
 			}
-			MPI_Ibcast(result, N-n_broadcast, MPI_DOUBLE, 0, MPI_COMM_WORLD, &requests_broadcast);
+			MPI_Ibcast(&result[0], N-n_broadcast, MPI_DOUBLE, 0, MPI_COMM_WORLD, &requests_broadcast);
 
 			while (true) {
-				MPI_Test(&requests_receive, &ready_receive, MPI_STATUS_IGNORE);
-				if (ready_receive) { /* task arrived */
-					MPI_Request requests_result;
-
-					uint64_t i = task[0];
-					uint64_t j = task[1];
-        			for (uint64_t w=0; w<i; ++w)
-        			    res += M[j*N+j+i-w-1] * M[(j+1+w)*N+j+i]; 
-        			M[j*N+j+i] = std::cbrt(res);
-					res = M[j*N+j+i];
-
-					//std::printf("%d mi è arrivata una task %ld %ld\n",myId, tasks[0], tasks[1]);
-					MPI_Isend(&res, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &requests_result);
-					MPI_Wait(&requests_result, MPI_STATUS_IGNORE);
-					//std::printf("%d ho mandato risultato \n",myId);
-					/* next possible task */
-					MPI_Irecv(task, 2, MPI_UINT64_T, 0, 0, MPI_COMM_WORLD, &requests_receive);
-					c = false; 
-				}				
-
 				MPI_Test(&requests_broadcast, &ready_broadcast, MPI_STATUS_IGNORE);
 				if (ready_broadcast) { 
-					n_broadcast++;
+					++n_broadcast;
 
-					for (uint64_t j=0; j<N-n_broadcast; j++) 
+					if (myId == 1)	
+						std::printf("\n\n%d e il numero  %ld N = %ld \n",myId,n_broadcast,N);
+					for (uint64_t j=0; j<N-n_broadcast; ++j) {
 						M[j*N+j+n_broadcast] = result[j];
+						if (myId == 1)	
+							std::printf("\n\n%d e j = %ld e scrive %f \n",myId,j,result[j]);
+					}
+					result.clear();
+					result.resize(N-n_broadcast-1);
 
+					
 					//std::printf("%d E' arrivato il broadcast %ld\n",myId, n_broadcast);
 					break;
+				} else {
+
+					MPI_Test(&requests_receive, &ready_receive, MPI_STATUS_IGNORE);
+					if (ready_receive) { /* task arrived */
+						MPI_Request requests_result;
+						uint64_t i = task[0];
+						uint64_t j = task[1];
+
+    	    			for (uint64_t w=0; w<i; ++w)
+    	    			    res += M[j*N+j+i-w-1] * M[(j+1+w)*N+j+i]; 
+    	    			res = std::cbrt(res);
+						if (myId == 1)	
+							print_M(&M,N);
+						std::printf("%d mi è arrivata una task %ld %ld e il risultato %f\n",myId, task[0], task[1],res);
+						MPI_Isend(&res, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &requests_result);
+						MPI_Wait(&requests_result, MPI_STATUS_IGNORE);
+						res = 0;
+						//std::printf("%d ho mandato risultato \n",myId);
+						/* next possible task */
+						MPI_Irecv(task, 2, MPI_UINT64_T, 0, 0, MPI_COMM_WORLD, &requests_receive);
+						c = false; 
+				}				
+
 				}
+
+				
 			}
 		}
+#ifdef SEQ
+    if (compare_M(&M,&M_test,N)) 
+        std::printf("Test Passed\n");
+    else
+        std::printf("Test Failed\n");
+#endif
 		//std::printf("\n\nSONO USCITO\n\n");
 	}
-	
-	//std::printf("\n\nTUTTI USCITI\n\n");
+
+	//print_M(&M,N);
+ 
+	if (myId == 0)	{
+		std::printf("\n\nM TEST \n\n");
+		print_M(&M_test,N);
+	}
+
+
+
 	MPI_Finalize();
 	return 0;
 }
